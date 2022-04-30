ffc193f25fee1c9530e6dbb140fd8294c13a7cc4
Oleksii Borshch <sergeevi44@gmail.com>
2022-04-30 13:15:33 +0300

    hw

--- local_storage/epidemies.js
+++ local_storage/epidemies.js
@@ -4,32 +4,23 @@ class Epidemy {
 		this.#deseases = deseases;
 	}
 
-	showEpidemies() {
+	getItems() {
 		for (const desease of this.#deseases) {
 			console.log(`name: ${desease.name}, link: ${desease.href}`);
 		}
 	}
 
-	addEpidemy(newEpidemy) {
+	readFromForm = () => {};
+
+	setItems(newEpidemy) {
 		this.#deseases = [...this.#deseases, newEpidemy];
 	}
-
-	updateEpidemy(name, newName) {
-		for (const desease of this.#deseases) {
-			if (desease.name === name) {
-				desease.name = newName;
-			} else {
-				console.log(`desease ${name} not found`);
-			}
-		}
-	}
-
-	removeEpidemy(name) {
-		for (const desease of this.#deseases) {
-			if (desease.name === name) {
-				const index = this.#deseases.indexOf(name);
-				this.#deseases.splice(index, 1);
-			}
-		}
-	}
 }
+
+const war = new Epidemy(deseases);
+war.addEpidemy({
+	name: "russia",
+	href: "https://russianembassyprotest.wordpress.com/2011/12/05/users-unite-around-the-globe-in-support-of-their-russian-peers/",
+});
+
+console.log(war);
